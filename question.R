da19636ebe1587bab25867fbb0e99580d0664791
Peimin <plin0018@student.monash.edu>
2020-10-14 21:29:40 +1100

    add tidy variable rmd

---
+++ question.R
@@ -0,0 +1,13 @@
+1. which day of month is most common, why?
+2. compare western countries which have Christmas and Asian country in Christmas period
+3.how many night for booking and which day in week is most popular with customer type and adult, children and babies.
+4.Is it more possible to cancel your booking if you have also booked full breakfast.
+and you are also bringing family. which month and days had the most cancellations. is it more likely for a resort or city hotel
+5. Is the variable average day price is influenced by the number of the day you stay and the type of the room you reserved?
+6. Which countries visit Portugal more often and how the number of bookings from that countries changes over time
+7.Which countries cancel their booking more often( to answer we can include a colored mop)
+8. which country s people most repeat reserve this hotel?
+9. how soon travellers book the hotel
+10. how soon travellers book the hotel? which hotel is more expensive for the same type of rooms and in which months
+11. what is the probability to cancel your booking if you have already cancel before
+12. How close to the arrival data are the booking being cancelled
